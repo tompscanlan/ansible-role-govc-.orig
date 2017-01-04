fbfc7d9ba73780c35f1755c4ebe338734784eb1d
Tom Scanlan <tscanlan@vmware.com>
2017-01-04 12:30:27 -0500

    initial
    Signed-off-by: Tom Scanlan <tscanlan@vmware.com>

--- tests/test.sh
+++ tests/test.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
+# Copyright © 2017 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: MIT
 
 set -e
-set -x
 
 containers=(
-    "ansible/ansible:ubuntu1204"
     "tompscanlan/photon-ansible-base"
     "ansible/ansible:fedora24"
+    "ansible/ansible:ubuntu1204"
     "ansible/ansible:ubuntu1604"
     "ansible/ansible:opensuse42.2"
     "ansible/ansible:centos7"
+
     )
 inits=(
+    "/usr/lib/systemd/systemd"
+    "/usr/lib/systemd/systemd"
     "/sbin/init"
-    "/usr/lib/systemd/systemd"
-    "/usr/lib/systemd/systemd"
     "/lib/systemd/systemd"
     "/usr/lib/systemd/systemd"
     "/usr/lib/systemd/systemd"
 )
 options=(
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
     ""
-    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
-    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
     "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
     "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
     "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
@@ -45,11 +47,15 @@ for ((i=0;i<${#containers[@]};++i)); do
 
     docker run  --rm  -d $option -v "$PWD:/etc/ansible/roles/ansible-role-govc" "$container" "$init" > "${container_id}"
 
-    docker exec "$(cat ${container_id})" bash -c 'pip install -U pip;'
+    #docker exec "$(cat ${container_id})" bash -c 'pip install -U pip;'
 
     docker exec "$(cat ${container_id})" bash -c 'pip install ansible;'
     docker exec "$(cat ${container_id})" bash -c 'ansible-playbook /etc/ansible/roles/ansible-role-govc/tests/test.yml --syntax-check'
-    docker exec "$(cat ${container_id})" bash -c 'ansible-playbook /etc/ansible/roles/ansible-role-govc/tests/test.yml;'
+    docker exec "$(cat ${container_id})" bash -c 'ansible-playbook /etc/ansible/roles/ansible-role-govc/tests/test.yml -vv;'
+
+    # verify govc installed
+    docker exec "$(cat ${container_id})" bash -c '/tmp/govc version'
+    docker exec "$(cat ${container_id})" bash -c '/usr/bin/govc version'
     docker kill "$(cat ${container_id})"
 done
 
