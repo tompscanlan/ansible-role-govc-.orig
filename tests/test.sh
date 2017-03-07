6534e5258a5629da6ba5024c2cdc4a0163a5a19d
Tom Scanlan <tscanlan@vmware.com>
2017-03-07 16:41:41 -0500

    add tests

---
+++ tests/test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+set -x
+
+containers=(
+    "ansible/ansible:ubuntu1204"
+    "tompscanlan/photon-ansible-base"
+    "ansible/ansible:fedora24"
+    "ansible/ansible:ubuntu1604"
+    "ansible/ansible:opensuse42.2"
+    "ansible/ansible:centos7"
+    )
+inits=(
+    "/sbin/init"
+    "/usr/lib/systemd/systemd"
+    "/usr/lib/systemd/systemd"
+    "/lib/systemd/systemd"
+    "/usr/lib/systemd/systemd"
+    "/usr/lib/systemd/systemd"
+)
+options=(
+    ""
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+    "--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+)
+
+cleanup() {
+    echo "Failed tests on $container"
+
+    docker kill "$(cat ${container_id})"
+}
+
+trap cleanup EXIT
+
+for ((i=0;i<${#containers[@]};++i)); do
+
+    container="${containers[i]}"
+    init="${inits[i]}"
+    option="${options[i]}"
+    container_id=$(mktemp)
+
+    docker run  --rm  -d $option -v "$PWD:/etc/ansible/roles/ansible-role-govc" "$container" "$init" > "${container_id}"
+
+    docker exec "$(cat ${container_id})" bash -c 'pip install -U pip;'
+
+    docker exec "$(cat ${container_id})" bash -c 'pip install ansible;'
+    docker exec "$(cat ${container_id})" bash -c 'ansible-playbook /etc/ansible/roles/ansible-role-govc/tests/test.yml --syntax-check'
+    docker exec "$(cat ${container_id})" bash -c 'ansible-playbook /etc/ansible/roles/ansible-role-govc/tests/test.yml;'
+    docker kill "$(cat ${container_id})"
+done
+
+trap - EXIT
+
